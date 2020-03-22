2ac5316ae88ff840cec9623e28b029e9c9c6b219
Cristian Baldi <bld.cris.96@gmail.com>
2020-03-22 12:33:59 +0100

    Finish update to Python 3

--- Application/databaseController.py
+++ Application/databaseController.py
@@ -4,105 +4,148 @@ from datetime import datetime
 import datetime as dt
 import apiViaggiotreno
 import train
-import loginInfo
+
+# import loginInfo
 
 
 def createTrain(trainNumber):
     now = dt.datetime.now()
 
     # Connecting to database
-    database = MySQLdb.connect("localhost","root",loginInfo.databasePWS())
+    # database = MySQLdb.connect("localhost","root",loginInfo.databasePWS())
+    database = MySQLdb.connect("localhost", "root", "password")
     cursor = database.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute("USE TRENOBOT;")
 
-
     # Search for the train number
-    row=cursor.execute("SELECT * FROM trains WHERE number = '%d'" %(int(trainNumber)))
-    dbLine=cursor.fetchone()
+    row = cursor.execute(
+        "SELECT * FROM trains WHERE number = '%d'" % (int(trainNumber))
+    )
+    dbLine = cursor.fetchone()
 
-    if(row==1 and ((now - dbLine['last_update']).total_seconds()/60.0) <1.5): # if the train is in DB and last update recent than 1,5 minutes (1:30 min)
-        usedTrain=trainObjFromDb(dbLine)
-
-    else: #else, I must update/add the train in DB
-        createdLine=addOrUpdateTrainDB(trainNumber)
-        if 'Error' in createdLine:
+    if (
+        row == 1 and ((now - dbLine["last_update"]).total_seconds() / 60.0) < 1.5
+    ):  # if the train is in DB and last update recent than 1,5 minutes (1:30 min)
+        usedTrain = trainObjFromDb(dbLine)
+    else:  # else, I must update/add the train in DB
+        createdLine = addOrUpdateTrainDB(trainNumber)
+        if "Error" in createdLine:
             database.close()
             return createdLine
 
-        usedTrain=trainObjFromDb(createdLine)
+        usedTrain = trainObjFromDb(createdLine)
 
     database.close()
     return usedTrain
 
 
 def addOrUpdateTrainDB(number):
-    jsonapi, raw_jsonapi, idTrain=apiViaggiotreno.realTimeInformation(number)
+    jsonapi, raw_jsonapi, idTrain = apiViaggiotreno.realTimeInformation(number)
 
-    if 'error' in idTrain:
-        return 'Error: il treno cercato non esiste :pensive:'
+    if "error" in idTrain:
+        return "Error: il treno cercato non esiste :pensive:"
 
     # Connecting to database
-    database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
+    # database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
+    database = MySQLdb.connect("localhost", "root", "password")
     cursor = database.cursor()
 
-    #Prepare name filed for INSERT record on DB
-    field_train_db="""id, number, origin, destination, departure_datetime, arrival_datetime, duration, delay, state, 
+    # Prepare name filed for INSERT record on DB
+    field_train_db = """id, number, origin, destination, departure_datetime, arrival_datetime, duration, delay, state, 
                         last_detection_time, last_detection_station, stations, alert, last_update"""
 
-    #Prepare content filed that needing changes compared to the original json by viaggiotreno
-    state=train_state(jsonapi)  #Deleted, regular..
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S") #Actual time, for the last_update filed
-    fermate=raw_jsonapi[raw_jsonapi.index('"fermate":')+10:raw_jsonapi.index('}],')+2] #list of all the stops, with binary and other info. I leave it in JSON.
+    # Prepare content filed that needing changes compared to the original json by viaggiotreno
+    state = train_state(jsonapi)  # Deleted, regular..
+    # print(state)
+    now = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )  # Actual time, for the last_update filed
+    fermate = raw_jsonapi[
+        raw_jsonapi.index('"fermate":') + 10 : raw_jsonapi.index("}],") + 2
+    ]  # list of all the stops, with binary and other info. I leave it in JSON.
 
-    #Prepare string for the content filed in INSERT record on DB
-    content_filed1=("'%s', %s, '%s', '%s', '%s', '%s', '%s',"%(idTrain, jsonapi['numeroTreno'], jsonapi['origineZero'], jsonapi['destinazioneZero'], convert_timestamp(jsonapi['orarioPartenza']), convert_timestamp(jsonapi['orarioArrivo']), jsonapi['compDurata']))
-    content_filed2=("%s, '%s', '%s', '%s', '%s', '%s', '%s'"%(jsonapi['ritardo'], state, convert_timestamp(jsonapi['oraUltimoRilevamento']), jsonapi['stazioneUltimoRilevamento'], fermate, jsonapi['subTitle'], now))
-    content_filed_db='%s %s'%(content_filed1, content_filed2)
+    # Prepare string for the content filed in INSERT record on DB
+    content_filed1 = "'%s', %s, '%s', '%s', '%s', '%s', '%s'," % (
+        idTrain,
+        jsonapi["numeroTreno"],
+        jsonapi["origineZero"],
+        jsonapi["destinazioneZero"],
+        convert_timestamp(jsonapi["orarioPartenza"]),
+        convert_timestamp(jsonapi["orarioArrivo"]),
+        jsonapi["compDurata"],
+    )
+    content_filed2 = "%s, '%s', '%s', '%s', '%s', '%s', '%s'" % (
+        jsonapi["ritardo"],
+        state,
+        convert_timestamp(jsonapi["oraUltimoRilevamento"]),
+        jsonapi["stazioneUltimoRilevamento"],
+        fermate,
+        jsonapi["subTitle"],
+        now,
+    )
+    content_filed_db = "%s %s" % (content_filed1, content_filed2)
 
-    #Insert the record into database "TRENOBOT", table "train"
+    # Insert the record into database "TRENOBOT", table "train"
     try:
         cursor.execute("USE TRENOBOT;")
-        cursor.execute("REPLACE INTO trains (%s) VALUES (%s);" %(field_train_db, content_filed_db)) #Use REPLACE instead of INSERT for update old records if exists
+        cursor.execute(
+            "REPLACE INTO trains (%s) VALUES (%s);" % (field_train_db, content_filed_db)
+        )  # Use REPLACE instead of INSERT for update old records if exists
         database.commit()
         database.close()
     except MySQLdb.Error as e:
         database.rollback()
         database.close()
-        return('Got Error {!r}, errno is {}'.format(e, e.args[0]))
+        return "Got Error {!r}, errno is {}".format(e, e.args[0])
 
-
-    database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
+    # database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
+    database = MySQLdb.connect("localhost", "root", "password")
     cursor = database.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute("USE TRENOBOT;")
-    cursor.execute("SELECT * FROM trains WHERE number = '%d'" %(int(number)))
-    createdLine=cursor.fetchone()
-    database.close()  #closing the db connection
+    cursor.execute("SELECT * FROM trains WHERE number = '%d'" % (int(number)))
+    createdLine = cursor.fetchone()
+    database.close()  # closing the db connection
 
-    return(createdLine)
+    return createdLine
 
-#Function for translate timestamp (used in viaggiotreno) in DATETIME format for mariaDB DATETIME filed
+
+# Function for translate timestamp (used in viaggiotreno) in DATETIME format for mariaDB DATETIME filed
 def convert_timestamp(timestamp):
     print(timestamp)
-    if(timestamp==None):
-        return '-'
+    if timestamp == None:
+        return "-"
     else:
-        datetime=dt.datetime.fromtimestamp(timestamp/1000)
+        datetime = dt.datetime.fromtimestamp(timestamp / 1000)
         return datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-#Function to interpret the train status from the codes
+
+# Function to interpret the train status from the codes
 def train_state(jsonapi):
-    if("PG" in jsonapi['tipoTreno']):
-        state='Regolare'
+    if "PG" in jsonapi["tipoTreno"]:
+        state = "Regolare"
     else:
-        if("ST" in jsonapi['tipoTreno']):
-            state='Soppresso :red_circle: :red_circle:'
+        if "ST" in jsonapi["tipoTreno"]:
+            state = "Soppresso :red_circle: :red_circle:"
         else:
-            state='Parzialmente Soppresso :red_circle:'
+            state = "Parzialmente Soppresso :red_circle:"
     return state
 
-def trainObjFromDb(dbLine): #create the Train object from the DB
-    usedTrain=train.Train(dbLine['id'], dbLine['number'], dbLine['origin'], dbLine['destination'], dbLine['departure_datetime'], dbLine['arrival_datetime'], dbLine['duration'], dbLine['delay'], dbLine['state'], dbLine['last_detection_time'], dbLine['last_detection_station'], dbLine['stations'], dbLine['alert'], dbLine['last_update'])
+
+def trainObjFromDb(dbLine):  # create the Train object from the DB
+    usedTrain = train.Train(
+        dbLine["id"],
+        dbLine["number"],
+        dbLine["origin"],
+        dbLine["destination"],
+        dbLine["departure_datetime"],
+        dbLine["arrival_datetime"],
+        dbLine["duration"],
+        dbLine["delay"],
+        dbLine["state"],
+        dbLine["last_detection_time"],
+        dbLine["last_detection_station"],
+        dbLine["stations"],
+        dbLine["alert"],
+        dbLine["last_update"],
+    )
     return usedTrain
-
-
-    
--- Application/trip_search.py
+++ Application/trip_search.py
@@ -5,116 +5,211 @@ import json
 import telepot
 from emoji import emojize
 import buttons
-import loginInfo
 
-TOKEN = loginInfo.telegramKey()
+# import loginInfo
+
+# TOKEN = loginInfo.telegramKey()
+TOKEN = ""
 bot = telepot.Bot(TOKEN)
 
-def trip_search (command, arrivo, partenza, mese, giorno, ora, data, now, chat_id):
 
-#Creo url per la richiesta del CODICE DELLE STAZIONI e interrogo
-    urlPartenza=('http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/%s' %partenza)
-    urlArrivo=('http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/%s' %arrivo)
-#Interrogo per ottenere il codice della stazione di PARTENZA
-    CodiceStazionePartenza=urllib.request.urlopen(urlPartenza).read()
+def trip_search(command, arrivo, partenza, mese, giorno, ora, data, now, chat_id):
 
-    if CodiceStazionePartenza=='':
-        return ("Stazione di <b>PARTENZA</b> non esistente.\n\n<b>CONSIGLI:</b>\n-Prova a scrivere solo le <b>prime parole chiave</b>, non il nome completo!\n\n-Oppure prova a scrivere il <b>nome completo!</b>\nEsempio: Milano Porta Garibaldi", "")
+    # Creo url per la richiesta del CODICE DELLE STAZIONI e interrogo
+    urlPartenza = (
+        "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/%s"
+        % partenza
+    )
+    urlArrivo = (
+        "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/%s"
+        % arrivo
+    )
+    # Interrogo per ottenere il codice della stazione di PARTENZA
+    CodiceStazionePartenza = urllib.request.urlopen(urlPartenza).read().decode("utf-8")
+
+    if CodiceStazionePartenza == "":
+        return (
+            "Stazione di <b>PARTENZA</b> non esistente.\n\n<b>CONSIGLI:</b>\n-Prova a scrivere solo le <b>prime parole chiave</b>, non il nome completo!\n\n-Oppure prova a scrivere il <b>nome completo!</b>\nEsempio: Milano Porta Garibaldi",
+            "",
+        )
     else:
-        NomeCompletoStazionePartenza=CodiceStazionePartenza[:CodiceStazionePartenza.index('|')]
-        if '\n' in CodiceStazionePartenza:
-            CodiceStazionePartenza=CodiceStazionePartenza[CodiceStazionePartenza.index('|')+2:CodiceStazionePartenza.index('\n')]
+        NomeCompletoStazionePartenza = CodiceStazionePartenza[
+            : CodiceStazionePartenza.index("|")
+        ]
+        if "\n" in CodiceStazionePartenza:
+            CodiceStazionePartenza = CodiceStazionePartenza[
+                CodiceStazionePartenza.index("|")
+                + 2 : CodiceStazionePartenza.index("\n")
+            ]
         else:
-            CodiceStazionePartenza=CodiceStazionePartenza[CodiceStazionePartenza.index('|')+2:]
+            CodiceStazionePartenza = CodiceStazionePartenza[
+                CodiceStazionePartenza.index("|") + 2 :
+            ]
 
-        while CodiceStazionePartenza[0]=='0':
-            CodiceStazionePartenza=CodiceStazionePartenza[1:]
+        while CodiceStazionePartenza[0] == "0":
+            CodiceStazionePartenza = CodiceStazionePartenza[1:]
 
-#Interrogo per ottenere il codice della stazione di ARRIVO
-    CodiceStazioneArrivo=urllib.request.urlopen(urlArrivo).read()
+    # Interrogo per ottenere il codice della stazione di ARRIVO
+    CodiceStazioneArrivo = urllib.request.urlopen(urlArrivo).read().decode("utf-8")
 
-    if CodiceStazioneArrivo=='':
-        return ("Errore\n\n-Prova a scrivere solo le <b>prime parole chiave</b>, non il nome completo!\nEsempio:\nMilano Porta\n\n-Oppure prova a scrivere il <b>nome completo!</b>\nEsempio: Milano Porta Garibaldi\n\n-Se il nome della stazione comprende piu' parole <b>usa i connettivi 'da' e 'a'</b> come in questo formato:\nDa Milano Centrale a Reggio Calabria\n\n<b>Esempi Corretti:</b>\n<i>Da Roma a Milano\nDa Roma a Milano il 10-8\nDa Roma a Milano alle 20:00\nDa Roma Tiburtina a Milano Lambrate il 10-8 alle 12:30\n</i>", "")
+    if CodiceStazioneArrivo == "":
+        return (
+            "Errore\n\n-Prova a scrivere solo le <b>prime parole chiave</b>, non il nome completo!\nEsempio:\nMilano Porta\n\n-Oppure prova a scrivere il <b>nome completo!</b>\nEsempio: Milano Porta Garibaldi\n\n-Se il nome della stazione comprende piu' parole <b>usa i connettivi 'da' e 'a'</b> come in questo formato:\nDa Milano Centrale a Reggio Calabria\n\n<b>Esempi Corretti:</b>\n<i>Da Roma a Milano\nDa Roma a Milano il 10-8\nDa Roma a Milano alle 20:00\nDa Roma Tiburtina a Milano Lambrate il 10-8 alle 12:30\n</i>",
+            "",
+        )
     else:
-        NomeCompletoStazioneArrivo=CodiceStazioneArrivo[:CodiceStazioneArrivo.index('|')]
-        if '\n' in CodiceStazioneArrivo:
-            CodiceStazioneArrivo=CodiceStazioneArrivo[CodiceStazioneArrivo.index('|')+2:CodiceStazioneArrivo.index('\n')]
+        NomeCompletoStazioneArrivo = CodiceStazioneArrivo[
+            : CodiceStazioneArrivo.index("|")
+        ]
+        if "\n" in CodiceStazioneArrivo:
+            CodiceStazioneArrivo = CodiceStazioneArrivo[
+                CodiceStazioneArrivo.index("|") + 2 : CodiceStazioneArrivo.index("\n")
+            ]
         else:
-            CodiceStazioneArrivo=CodiceStazioneArrivo[CodiceStazioneArrivo.index('|')+2:]
-        while CodiceStazioneArrivo[0]=='0':
-            CodiceStazioneArrivo=CodiceStazioneArrivo[1:]
+            CodiceStazioneArrivo = CodiceStazioneArrivo[
+                CodiceStazioneArrivo.index("|") + 2 :
+            ]
+        while CodiceStazioneArrivo[0] == "0":
+            CodiceStazioneArrivo = CodiceStazioneArrivo[1:]
 
-#Interrogo con codici delle stazioni di partenza e ritorno e data e ora CODICEPARTENZA/CODICEARRIVO/AAA-MM-GGTHH:MM:SS occhio alla T che separa data e ora
-    DataOra='%s-%s-%sT%s:00:00'%(now.year, mese, giorno, ora)
-    urlRicerca=('http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/%s/%s/%s' % (CodiceStazionePartenza, CodiceStazioneArrivo, DataOra))
+    # Interrogo con codici delle stazioni di partenza e ritorno e data e ora CODICEPARTENZA/CODICEARRIVO/AAA-MM-GGTHH:MM:SS occhio alla T che separa data e ora
+    DataOra = "%s-%s-%sT%s:00:00" % (now.year, mese, giorno, ora)
+    urlRicerca = (
+        "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/%s/%s/%s"
+        % (CodiceStazionePartenza, CodiceStazioneArrivo, DataOra)
+    )
     print(urlRicerca)
-    #bot.sendMessage(chat_id, 'UrlRicerca: %s' %(urlRicerca))
-    parsed_json = json.loads(urllib.request.urlopen(urlRicerca).read())
+    # bot.sendMessage(chat_id, 'UrlRicerca: %s' %(urlRicerca))
+    parsed_json = json.loads(urllib.request.urlopen(urlRicerca).read().decode("utf-8"))
     try:
-        for sol in range (0,2): #ciclo che fa visualizzare le prime 2 opzioni
-            risp=""
-            trains=[]
-            departure_time_keyboard=[]
-            arrival_time_keyboard=[]
-            departure=[]
-            arrival=[]
-            for cont in range(0,len(parsed_json['soluzioni'][sol]['vehicles'])): #ciclo che crea il messaggio per quando la soluzione ha piu treni
-                departure_time=parsed_json['soluzioni'][sol]['vehicles'][cont]['orarioPartenza']
-                departure_day=departure_time[departure_time.index('T')-2:departure_time.index('T')]
-                departure_month=departure_time[departure_time.index('-')+1:]
-                departure_month=departure_month[:departure_time.index('-')-2]
-                departure_time=departure_time[departure_time.index('T')+1:departure_time.index('T')+6]
-                arrival_time=parsed_json['soluzioni'][sol]['vehicles'][cont]['orarioArrivo']
-                arrival_time=arrival_time[arrival_time.index('T')+1:arrival_time.index('T')+6]
+        for sol in range(0, 2):  # ciclo che fa visualizzare le prime 2 opzioni
+            risp = ""
+            trains = []
+            departure_time_keyboard = []
+            arrival_time_keyboard = []
+            departure = []
+            arrival = []
+            for cont in range(
+                0, len(parsed_json["soluzioni"][sol]["vehicles"])
+            ):  # ciclo che crea il messaggio per quando la soluzione ha piu treni
+                departure_time = parsed_json["soluzioni"][sol]["vehicles"][cont][
+                    "orarioPartenza"
+                ]
+                departure_day = departure_time[
+                    departure_time.index("T") - 2 : departure_time.index("T")
+                ]
+                departure_month = departure_time[departure_time.index("-") + 1 :]
+                departure_month = departure_month[: departure_time.index("-") - 2]
+                departure_time = departure_time[
+                    departure_time.index("T") + 1 : departure_time.index("T") + 6
+                ]
+                arrival_time = parsed_json["soluzioni"][sol]["vehicles"][cont][
+                    "orarioArrivo"
+                ]
+                arrival_time = arrival_time[
+                    arrival_time.index("T") + 1 : arrival_time.index("T") + 6
+                ]
 
-                #keyboard generate
+                # keyboard generate
 
-                trains.append(parsed_json['soluzioni'][sol]['vehicles'][cont]['numeroTreno'])
-                departure.append(parsed_json['soluzioni'][sol]['vehicles'][cont]['origine'])
-                arrival.append(parsed_json['soluzioni'][sol]['vehicles'][cont]['destinazione'])
+                trains.append(
+                    parsed_json["soluzioni"][sol]["vehicles"][cont]["numeroTreno"]
+                )
+                departure.append(
+                    parsed_json["soluzioni"][sol]["vehicles"][cont]["origine"]
+                )
+                arrival.append(
+                    parsed_json["soluzioni"][sol]["vehicles"][cont]["destinazione"]
+                )
                 departure_time_keyboard.append(departure_time)
                 arrival_time_keyboard.append(arrival_time)
 
+                # appendo i vari treni della soluzione in coda al messaggio di risposta
+                risp += (
+                    ":train2: Treno %s %s\n:arrow_right: <b>%s</b> da %s\n:arrow_left: <b>%s</b> a %s\n\n"
+                    % (
+                        parsed_json["soluzioni"][sol]["vehicles"][cont][
+                            "categoriaDescrizione"
+                        ],
+                        parsed_json["soluzioni"][sol]["vehicles"][cont]["numeroTreno"],
+                        departure_time,
+                        parsed_json["soluzioni"][sol]["vehicles"][cont]["origine"],
+                        arrival_time,
+                        parsed_json["soluzioni"][sol]["vehicles"][cont]["destinazione"],
+                    )
+                )
+            response = risp = (
+                ":white_check_mark:<b>Soluzione %s</b>\n\n:calendar: %s-%s-%s\n:clock10:Durata <b>%s</b>\n\n%s"
+                % (
+                    sol + 1,
+                    departure_day,
+                    departure_month,
+                    now.year,
+                    parsed_json["soluzioni"][sol]["durata"],
+                    risp,
+                )
+            )
 
-
-
-                #appendo i vari treni della soluzione in coda al messaggio di risposta
-                risp+=(':train2: Treno %s %s\n:arrow_right: <b>%s</b> da %s\n:arrow_left: <b>%s</b> a %s\n\n' %(parsed_json['soluzioni'][sol]['vehicles'][cont]['categoriaDescrizione'], parsed_json['soluzioni'][sol]['vehicles'][cont]['numeroTreno'], departure_time, parsed_json['soluzioni'][sol]['vehicles'][cont]['origine'], arrival_time, parsed_json['soluzioni'][sol]['vehicles'][cont]['destinazione']))
-            response= risp=':white_check_mark:<b>Soluzione %s</b>\n\n:calendar: %s-%s-%s\n:clock10:Durata <b>%s</b>\n\n%s' %(sol+1, departure_day, departure_month, now.year, parsed_json['soluzioni'][sol]['durata'], risp)
-
-            keyboard=buttons.searchResultMenuButtons(chat_id, trains, departure_time_keyboard, arrival_time_keyboard, departure, arrival)
-            bot.sendMessage(chat_id, emojize(response, use_aliases=True), parse_mode='html', disable_web_page_preview=None, disable_notification=None, reply_markup=keyboard)
-    except Exception as e: 
+            keyboard = buttons.searchResultMenuButtons(
+                chat_id,
+                trains,
+                departure_time_keyboard,
+                arrival_time_keyboard,
+                departure,
+                arrival,
+            )
+            bot.sendMessage(
+                chat_id,
+                emojize(response, use_aliases=True),
+                parse_mode="html",
+                disable_web_page_preview=None,
+                disable_notification=None,
+                reply_markup=keyboard,
+            )
+    except Exception as e:
         print(e)
-        keyboard=buttons.backMainMenuButtons()
-        bot.sendMessage(chat_id, emojize("Le API di viaggiotreno non sono in grado di rispondere a questa richeiesta anche se il tuo comando e' valido.\n\nIl motivo e' sconosciuto e da attributire a viaggiatreno.it\n\nCerca qui il tuo treno: http://www.trenitalia.com/ \n\nQuando sai il numero del tuo treno torna qui!", use_aliases=True), parse_mode='html', disable_web_page_preview=None, disable_notification=None, reply_markup=keyboard)
+        keyboard = buttons.backMainMenuButtons()
+        bot.sendMessage(
+            chat_id,
+            emojize(
+                "Le API di viaggiotreno non sono in grado di rispondere a questa richeiesta anche se il tuo comando e' valido.\n\nIl motivo e' sconosciuto e da attributire a viaggiatreno.it\n\nCerca qui il tuo treno: http://www.trenitalia.com/ \n\nQuando sai il numero del tuo treno torna qui!",
+                use_aliases=True,
+            ),
+            parse_mode="html",
+            disable_web_page_preview=None,
+            disable_notification=None,
+            reply_markup=keyboard,
+        )
 
 
-def date_parser (command):
+def date_parser(command):
 
     now = datetime.datetime.now()
 
-    output=""
+    output = ""
 
-    if('-' not in command and '/' not in command):
-        output="%s-%s" %(command, now.month)
+    if "-" not in command and "/" not in command:
+        output = "%s-%s" % (command, now.month)
     else:
-        if('/' in command):
-            output=command.replace("/", "-")
+        if "/" in command:
+            output = command.replace("/", "-")
         else:
-            output=command
+            output = command
 
-    if(command==""):
-        output='%s-%s' %(now.day, now.month)
+    if command == "":
+        output = "%s-%s" % (now.day, now.month)
 
-    giorno=output[:output.index("-")]
-    mese=output[output.index("-")+1:]
-    if(int(giorno)>31):
-        #il Giorno inserito e' inesistente. Verra' utilizzato il GIORNO ODIERNO
-        output='%s-%s' %(now.day, now.month)
+    giorno = output[: output.index("-")]
+    mese = output[output.index("-") + 1 :]
+    if int(giorno) > 31:
+        # il Giorno inserito e' inesistente. Verra' utilizzato il GIORNO ODIERNO
+        output = "%s-%s" % (now.day, now.month)
 
-    if(int(mese)>12):
-        return ("Attenzione, il Mese inserito e' inesistente.\n\\nMese ricevuto = %s\n\nVerra' utilizzato il MESE ODIERNO" %(mese), "")
+    if int(mese) > 12:
+        return (
+            "Attenzione, il Mese inserito e' inesistente.\n\\nMese ricevuto = %s\n\nVerra' utilizzato il MESE ODIERNO"
+            % (mese),
+            "",
+        )
 
     return output
-
