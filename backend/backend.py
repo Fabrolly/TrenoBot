cae29225a43e7ec4f2f45d5b49e2c6fa9b7017b6
Fabrizio Olivadese <fabry.olivadese@gmail.com>
2020-03-22 17:18:00 +0100

    Using request instead of urlib

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
-import urllib.request, urllib.parse, urllib.error
-import json
+import requests
 
 app = Flask(__name__)
 
@@ -8,27 +7,22 @@ app = Flask(__name__)
 # For the requets both the Number and Station_ID are necessary for the query. Then i must have the Statation_ID from the number given by user
 def trainId(train_number):
     try:
-        autocomplete = (
-            urllib.request.urlopen(
-                "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/cercaNumeroTrenoTrenoAutocomplete/%s"
-                % train_number
-            )
-            .read()
-            .decode("utf-8")
-        )
+        response = requests.get('http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/cercaNumeroTrenoTrenoAutocomplete/%s' % train_number)
+        response.encoding = 'utf-8'
+        response = response.text
     except Exception as e:
         return "error"  # probably the trenitalia API are offline. I must handle this situation
 
-    if len(autocomplete) != 0 and "<H1>" not in autocomplete:
+    if len(response) != 0 and "<H1>" not in response:
         if (
-            "-" in autocomplete
+            "-" in response
         ):  # this thing is necessary because for some reasons there are train with the same number but different station_ID.
-            autocomplete = autocomplete[(autocomplete.index("|") + 1) :]
-            autocomplete = autocomplete[
-                (autocomplete.index("-") + 1) : (autocomplete.index("\n"))
+            response = response[(response.index("|") + 1) :]
+            response = response[
+                (response.index("-") + 1) : (response.index("\n"))
             ]
             # print(autocomplete)
-            return autocomplete  # return the ID of the train
+            return response  # return the ID of the train
         else:
             return "error"
 
@@ -38,7 +32,6 @@ def trainId(train_number):
 def realTimeInformation(number):
     try:
         idTrain = trainId(number)  # I need also the Station_ID for the request
-        print(idTrain)
         if "error" in idTrain:
             return "", "", "error"
         url = "%s/%s" % (
@@ -46,15 +39,12 @@ def realTimeInformation(number):
             number,
         )  # See api docs: the request urls is composed by TRAIN_ID/TRAIN_NUMBER
         raw_json = (
-            urllib.request.urlopen(
+            requests.get(
                 "http://www.viaggiatreno.it/viaggiatrenomobile/resteasy/viaggiatreno/andamentoTreno/%s"
                 % url
             )
-            .read()
-            .decode("utf-8")
         )
-        parsed_json = json.loads(raw_json)
-        return jsonify(parsed_json)
+        return jsonify(raw_json.json())
     except Exception as e:
         return e
 
