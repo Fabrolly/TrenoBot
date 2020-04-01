d063beed8bd79458c6f3b566802e98444f67af4a
Coich <coich1996@gmail.com>
2020-04-01 12:12:14 +0200

    Fixing formatting

--- backend/database_initialization.py
+++ backend/database_initialization.py
@@ -6,27 +6,29 @@ import warnings
 warnings.filterwarnings("ignore", category=MySQLdb.Warning)
 # ♦import loginInfo
 
+
 def database_initialization():
-	# Connecting to database as root
-	# database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
-	database = MySQLdb.connect("localhost", "root", "password")
-	cursor = database.cursor()
+    # Connecting to database as root
+    # database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
+    database = MySQLdb.connect("localhost", "root", "password")
+    cursor = database.cursor()
 
-	# If the TRENOBOT database doses not exist, create and open it (if already exist a warning will be generated)
-	cursor.execute("CREATE DATABASE IF NOT EXISTS TRAINSTATISTICS;")
-	cursor.execute("use TRAINSTATISTICS;")
+    # If the TRENOBOT database doses not exist, create and open it (if already exist a warning will be generated)
+    cursor.execute("CREATE DATABASE IF NOT EXISTS TRAINSTATISTICS;")
+    cursor.execute("use TRAINSTATISTICS;")
 
-	# If tables doses not exist, create it (if already exist a warning will be generated)
-	cursor.execute(
-	    "CREATE TABLE IF NOT EXISTS trains (trainID INT PRIMARY KEY, number TEXT, origin TEXT, destination TEXT, stations JSON, departure_datetime DATETIME, arrival_datetime DATETIME, duration INT)"
-	)
-	cursor.execute(
-	    "CREATE TABLE IF NOT EXISTS journeys (date DATETIME, trainID INT, real_departure_datetime DATETIME, real_arrival_datetime DATETIME, delay INT, state TEXT, alert TEXT, last_detection_datetime DATETIME, last_detection_station TEXT, last_update DATETIME, final_status TEXT, final_delay INT, PRIMARY KEY(date, trainID))"
-	)
+    # If tables doses not exist, create it (if already exist a warning will be generated)
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS trains (trainID INT PRIMARY KEY, number TEXT, origin TEXT, destination TEXT, stations JSON, departure_datetime DATETIME, arrival_datetime DATETIME, duration INT)"
+    )
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS journeys (date DATETIME, trainID INT, real_departure_datetime DATETIME, real_arrival_datetime DATETIME, delay INT, state TEXT, alert TEXT, last_detection_datetime DATETIME, last_detection_station TEXT, last_update DATETIME, final_status TEXT, final_delay INT, PRIMARY KEY(date, trainID))"
+    )
 
-	database.commit()
+    database.commit()
 
-	# Disconnecting
-	database.close()
+    # Disconnecting
+    database.close()
+
 
 database_initialization()
--- backend/test_database.py
+++ backend/test_database.py
@@ -1,6 +1,7 @@
 import sys
 from os import path
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 import datetime as dt
 import unittest
@@ -16,6 +17,7 @@ def delete_database():
     database.commit()
     database.close()
 
+
 def show_database():
     cursor = cursor_database()
     cursor.execute("show databases;")
@@ -24,86 +26,94 @@ def show_database():
         print(databases[0])
     return list(cursor)
 
+
 def execute_query(table_name):
     cursor = cursor_database()
     cursor.execute("use TRAINSTATISTICS;")
     cursor.execute(f"SELECT * FROM {table_name}")
-    print('Righe in Trains:')
+    print("Righe in Trains:")
     for query_element in cursor:
         print(query_element)
     return list(cursor)
+
 
 def cursor_database():
     database = MySQLdb.connect("localhost", "root", "password")
     return database.cursor()
 
 
-
-#class TestMssageParser(unittest.TestCase):
+# class TestMssageParser(unittest.TestCase):
 class Monolithic(unittest.TestCase):
-
     def setUp(self):
         delete_database()
 
     def test_create_database(self):
 
-        print('\n\nTEST - CREAZIONE DATABASE\n')
+        print("\n\nTEST - CREAZIONE DATABASE\n")
         databases = show_database()
         self.assertEqual(len(databases), 3)
 
         db_inizialization()
 
-        print('\nCREAZIONE E INIZIALIZZAZIONE DATABASE\n')
+        print("\nCREAZIONE E INIZIALIZZAZIONE DATABASE\n")
 
         databases = show_database()
         self.assertEqual(len(databases), 4)
 
-        print('\nTEST PASSED')
+        print("\nTEST PASSED")
 
     def test_insert_data(self):
 
-        print('\n\nTEST - INSERIMENTO RIGA\n')
+        print("\n\nTEST - INSERIMENTO RIGA\n")
         db_inizialization()
         database = MySQLdb.connect("localhost", "root", "password")
         cursor = database.cursor()
         cursor.execute("use TRAINSTATISTICS;")
 
-        query_result = execute_query('trains')
+        query_result = execute_query("trains")
         self.assertEqual(len(query_result), 0)
 
-        departure_datetime = dt.datetime.strptime("2020-03-31 16:08:00", "%Y-%m-%d %H:%M:%S")
-        arrival_datetime = dt.datetime.strptime("2020-03-31 16:48:00", "%Y-%m-%d %H:%M:%S")
+        departure_datetime = dt.datetime.strptime(
+            "2020-03-31 16:08:00", "%Y-%m-%d %H:%M:%S"
+        )
+        arrival_datetime = dt.datetime.strptime(
+            "2020-03-31 16:48:00", "%Y-%m-%d %H:%M:%S"
+        )
 
-        #stations = [{"Bergamo" : "16:08"}, {"Ponte S.Pietro" : "16:14"}, {"Cisano Caprino Berga" : "16:27"}, {"Lecco" : "16:48"}]
+        # stations = [{"Bergamo" : "16:08"}, {"Ponte S.Pietro" : "16:14"}, {"Cisano Caprino Berga" : "16:27"}, {"Lecco" : "16:48"}]
         stations = {}
 
         insert_query = f"INSERT INTO trains (trainID, number, origin, destination, stations, departure_datetime, arrival_datetime, duration) VALUES (5050, 'S01529', 'Bergamo', 'Lecco', \"{stations}\", \"{departure_datetime}\", \"{arrival_datetime}\", 40);"
         cursor.execute(insert_query)
         database.commit()
         database.close()
-        print('\nAGGIUNTA ESEGUITA\n')
+        print("\nAGGIUNTA ESEGUITA\n")
 
-        query_result = execute_query('trains')
+        query_result = execute_query("trains")
         self.assertEqual(len(query_result), 1)
 
-        print('\nTEST PASSED')
+        print("\nTEST PASSED")
 
     def test_delete_data(self):
 
-        print('\n\nTEST - CANCELLAMENTO RIGA\n')
+        print("\n\nTEST - CANCELLAMENTO RIGA\n")
         db_inizialization()
         database = MySQLdb.connect("localhost", "root", "password")
         cursor = database.cursor()
         cursor.execute("use TRAINSTATISTICS;")
 
-        departure_datetime = dt.datetime.strptime("2020-03-31 16:08:00", "%Y-%m-%d %H:%M:%S")
-        arrival_datetime = dt.datetime.strptime("2020-03-31 16:48:00", "%Y-%m-%d %H:%M:%S")
+        departure_datetime = dt.datetime.strptime(
+            "2020-03-31 16:08:00", "%Y-%m-%d %H:%M:%S"
+        )
+        arrival_datetime = dt.datetime.strptime(
+            "2020-03-31 16:48:00", "%Y-%m-%d %H:%M:%S"
+        )
         stations = {}
         insert_query = f"INSERT INTO trains (trainID, number, origin, destination, stations, departure_datetime, arrival_datetime, duration) VALUES (5050, 'S01529', 'Bergamo', 'Lecco', \"{stations}\", \"{departure_datetime}\", \"{arrival_datetime}\", 40);"
         cursor.execute(insert_query)
         database.commit()
 
-        query_result = execute_query('trains')
+        query_result = execute_query("trains")
         self.assertEqual(len(query_result), 1)
 
         delete_query = "DELETE FROM trains WHERE trainID = 5050"
@@ -111,12 +121,13 @@ class Monolithic(unittest.TestCase):
         database.commit()
         database.close()
 
-        print('\nRIMOZIONE ESEGUITA\n')
+        print("\nRIMOZIONE ESEGUITA\n")
 
-        query_result = execute_query('trains')
+        query_result = execute_query("trains")
         self.assertEqual(len(query_result), 0)
 
-        print('\nTEST PASSED')
+        print("\nTEST PASSED")
+
 
 # launch unit test cases
 if __name__ == "__main__":
