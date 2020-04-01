9ef47b8b6364df355cd3d8ce339eeb290e19951e
Coich <coich1996@gmail.com>
2020-04-01 11:31:18 +0200

    Added test case for database

--- backend/database_initialization.py
+++ backend/database_initialization.py
@@ -6,34 +6,27 @@ import warnings
 warnings.filterwarnings("ignore", category=MySQLdb.Warning)
 # ♦import loginInfo
 
-# Connecting to database as root
-# database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
-database = MySQLdb.connect("localhost", "root", "password")
-cursor = database.cursor()
+def database_initialization():
+	# Connecting to database as root
+	# database = MySQLdb.connect("localhost","root", loginInfo.databasePWS())
+	database = MySQLdb.connect("localhost", "root", "password")
+	cursor = database.cursor()
 
-# Print all the databases in the system (for debug purpose only, can be removed)
-cursor.execute("show databases;")
-print("\nEXISTING DATABASE:")
-for databases in cursor:
-    print(databases[0])
+	# If the TRENOBOT database doses not exist, create and open it (if already exist a warning will be generated)
+	cursor.execute("CREATE DATABASE IF NOT EXISTS TRAINSTATISTICS;")
+	cursor.execute("use TRAINSTATISTICS;")
 
-# If the TRENOBOT database doses not exist, create and open it (if already exist a warning will be generated)
-cursor.execute("CREATE DATABASE IF NOT EXISTS TRAINSTATISTICS;")
-cursor.execute("use TRAINSTATISTICS;")
+	# If tables doses not exist, create it (if already exist a warning will be generated)
+	cursor.execute(
+	    "CREATE TABLE IF NOT EXISTS trains (trainID INT PRIMARY KEY, number TEXT, origin TEXT, destination TEXT, stations JSON, departure_datetime DATETIME, arrival_datetime DATETIME, duration INT)"
+	)
+	cursor.execute(
+	    "CREATE TABLE IF NOT EXISTS journeys (date DATETIME, trainID INT, real_departure_datetime DATETIME, real_arrival_datetime DATETIME, delay INT, state TEXT, alert TEXT, last_detection_datetime DATETIME, last_detection_station TEXT, last_update DATETIME, final_status TEXT, final_delay INT, PRIMARY KEY(date, trainID))"
+	)
 
-# If tables doses not exist, create it (if already exist a warning will be generated)
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS trains (trainID INT PRIMARY KEY, number INT, origin TEXT, destination TEXT, stations JSON, departure_datetime DATETIME, arrival_datetime DATETIME, duration INT)"
-)
-cursor.execute(
-    "CREATE TABLE IF NOT EXISTS journeys (date DATETIME, trainID INT, real_departure_datetime DATETIME, real_arrival_datetime DATETIME, delay INT, state TEXT, alert TEXT, last_detection_datetime DATETIME, last_detection_station TEXT, last_update DATETIME, final_status TEXT, final_delay INT, PRIMARY KEY(date, trainID))"
-)
-cursor.execute("show tables;")
-print("\nTRAINSTATISTICS - TABLES:")
-for table in cursor:
-    print(table[0])
+	database.commit()
 
-database.commit()
+	# Disconnecting
+	database.close()
 
-# Disconnecting
-database.close()
+database_initialization()
